33e1da518bbefaea871313593e9b111b0b0fb198
AyenMejorada <mejoradaayen1102@gmail.com>
2025-07-12 21:20:36 +0800

    first commit

---
+++ Python-1_Introduction-to-Syntax/main.py
@@ -0,0 +1,23 @@
+# 1. variable of different types
+name = "Ayen"
+age = 18
+height = 5.1
+is_Student = True
+
+# 2. basic arithmetic operations
+sum_result = age + height
+product_result = age * height
+
+# 3. concatenate strings and display outputs
+greeting = "Hello, " + name + "! ^^"
+print(greeting)
+
+# 4. display results of arithmetic operations
+print("Sum of age and height is:", sum_result)
+print("Product of age and height is:", product_result)
+
+# 5. display types of each variable
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_Student", type(is_Student))
