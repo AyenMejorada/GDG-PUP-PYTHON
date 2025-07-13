7a19d366b04430f105ceba7cadee86d20040a76a
AyenMejorada <mejoradaayen1102@gmail.com>
2025-07-13 10:52:17 +0800

    Assignment 6

--- Python-6_Dictionaries/main.py
+++ Python-6_Dictionaries/main.py
@@ -1,20 +1,19 @@
-# creating dictionary
+# 1. creating dictionary
 # it is called a dictionary since like a real worl dictionary, they use key and a value
 # example in this code: key = name, value = Sparky
 person = {'name': 'Sparky', 'age': 25}
 print("Original dictionary:", person)
 
-# update
-# add new key-value pair
+# 2. add new key-value pair
 person['city'] = 'New York'
 print("Dictionary after adding an item:", person)
 
+# 3. update the exisitng key-value pair
 # update value of 'age' from 25 to 26
-# update the exisitng key-value pair
 person['age'] = 26
 print("Dictionary after updating an item:", person)
 
+# 4. remove a key-value pair
 # del statement to remove key value 'age'
-# remove a key-value pair
 del person['age']
 print("Dictionary after removing an item:", person)
